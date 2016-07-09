74da9a0222d8c1633df78fa50272d35e311de1de
DoubleKilled <ronkainen.eero@gmail.com>
2016-07-09 15:44:01 +0300

    Started work on more complex filtering

---
+++ AnimeChanger/Ani/FilterTypes/Types.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeChanger.Ani.FilterTypes
+{
+    public struct Replace
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+
+    public struct Whitelist
+    {
+        public string Keyword { get; set; }
+        public bool Found { get; set; }
+    }
+
+    public struct Blacklist
+    {
+        public string Keyword { get; set; }
+        public bool Found { get; set; }
+    }
+}
