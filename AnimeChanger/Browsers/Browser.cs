977fad0a2a0ba0923fe15a21faa27a82b729b2e0
DoubleKilled <ronkainen.eero@gmail.com>
2016-08-03 17:35:32 +0300

    Start on URL lookup

---
+++ AnimeChanger/Browsers/Browser.cs
@@ -0,0 +1,13 @@
+﻿namespace AnimeChanger
+{
+    /// <summary>
+    /// Class structure for supported browsers.
+    /// </summary>
+    public interface Browser
+    {
+        string ProcessName { get; set; }
+        string[] RemoveBrowserTitles { get; set; }
+
+        string getURL();
+    }
+}
---
+++ AnimeChanger/Browsers/Types/Chrome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeChanger.Browsers.Types
+{
+    public class Chrome : Browser
+    {
+        string ProcessName = "";
+        public string[] RemoveBrowserTitles = { "" };
+
+        public string getURL()
+        {
+            return null;
+        }
+    }
+}
