4a6e4ba01f4fb0cd4fbf17ef592753fc415f102b
DoubleKilled <ronkainen.eero@gmail.com>
2016-08-03 19:27:31 +0300

    URL lookup for chrome

---
+++ AnimeChanger/Browser/Browser.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace AnimeChanger.Browser
+{
+    public interface Browser
+    {
+        string ProcessName { get; set; }
+        string[] RemoveBrowserTitles { get; set; }
+
+        string getURL(Process process);
+    }
+}
---
+++ AnimeChanger/Browser/Supported/Chrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Automation;
+
+namespace AnimeChanger.Browser.Supported
+{
+    public class Chrome : Browser
+    {
+        public string ProcessName { get; set; }
+        public string[] RemoveBrowserTitles { get; set; }
+
+        public string getURL(Process process)
+        {
+            AutomationElement elm = AutomationElement.FromHandle(process.MainWindowHandle);
+            AutomationElement elmUrlBar = elm.FindFirst(TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.NameProperty, "Address and search bar"));
+
+            if (elmUrlBar != null)
+            {
+                AutomationPattern[] patterns = elmUrlBar.GetSupportedPatterns();
+                if (patterns.Length > 0)
+                {
+                    ValuePattern val = (ValuePattern)elmUrlBar.GetCurrentPattern(patterns[0]);
+                    return val.Current.Value;
+                }
+            }
+
+            return null;
+        }
+    }
+}
